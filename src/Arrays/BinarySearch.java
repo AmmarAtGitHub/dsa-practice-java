717562cc44db42c167f577fdd5f5b805246a814a
Ammar Shams Eddin <128472464+AmmarAtGitHub@users.noreply.github.com>
2025-04-18 23:06:51 +0200

    Update BinarySearch.java
    docs: add explanatory comments about how to avoid integer overflow when calculating the mid value

--- src/Arrays/BinarySearch.java
+++ src/Arrays/BinarySearch.java
@@ -5,7 +5,7 @@ public class BinarySearch{
     public static int search(int[] arr, int target) {
     int left = 0, right = arr.length -1;
     while (left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = left + (right - left) / 2; //  is a safer than (left + right)/2 to calculate the middle index in binary search, especially for large arrays, because it prevents integer overflow.
 
         if (arr[mid] == target) return mid; // target found as mid index
         else if (arr[mid] < target) left = mid +1;
