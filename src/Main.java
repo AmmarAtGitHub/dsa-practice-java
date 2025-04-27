bf90760dc793a49d393dfc63e74367b446ae4592
AmmarAtGitHub <ammar.shams.eddin@gmail.com>
2025-04-27 22:54:15 +0200

    tested the bubble sort method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import Arrays.BinarySearch; // import the BinarySearch class from the Arrays package
 import Arrays.RotatedBinarySearch; // import the RotatedBinarySearch class from the Arrays package
-
+import Arrays.BubbleSort; // import the BubbleSort class from the Arrays package
 public class Main{
 
     public static void main(String[] args){
@@ -9,7 +9,8 @@ public class Main{
         System.out.println("Running rotated binary search algorithm...");
         RotatedBinarySearch.run(); // call the run method to test the rotated binary search algorithm
         System.out.println("All algorithms have been executed.");
-
+        System.out.println("Running bubble sort algorithm...");
+        BubbleSort.run(); // call the run method to test the bubble sort algorithm
     }
 
 }
